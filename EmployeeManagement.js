02a9549961cddd242572d44663250c77bb23b52a
adeankush3 <adeankush3@gmail.com>
2022-03-22 21:59:31 +0530

    [Ankush]Employee Attendance

--- EmployeeManagement.js
+++ EmployeeManagement.js
@@ -1,1 +1,15 @@
 console.log("Welcome To Employee Wage Computation Using JS ");
+
+
+const IS_ABSENT =0;
+
+let checkStatus  = Math.floor(Math.random() * 10) % 2;
+
+if(checkStatus == IS_ABSENT)
+{
+    console.log("Employee is Absent");
+}
+else
+{
+    console.log("Employee is Present");
+}
