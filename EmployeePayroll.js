c82509799813f97b52339831ffd582a259d7b48d
adeankush3 <adeankush3@gmail.com>
2022-03-25 22:26:16 +0530

    [Ankush]Extend Gender And Start Date

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -3,22 +3,29 @@ class EmployeePayroll
     //Property
     id;
     salary;
-    
+    gender;
+    startDate;
     //Constructor
-    constructor(id, name,salary)
+    constructor(...params)
     {
-        this.id  = id;
-        this.name = name;
-        this.salary = salary;
+        this.id  = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     get name(){return this._name;}
     set name(name){this._name = name;}
 
     toString(){
-        return "id="+ this.id + ", name="+this.name+", salary="+this.salary;
+        const Option = {year: 'numeric', month: 'numeric', day:'numeric'};
+        const empDate = this.startDate === undefined ? "undefined":
+                         this.startDate.toLocaleDateString("en-US",Option);
+
+        return "id="+ this.id + ", name="+this.name+", salary="+this.salary+", Gender="+this.gender+", StartDate="+empDate;
     }
 }
 
-let employeePayroll = new EmployeePayroll(1,"Ankush",55000);
+let employeePayroll = new EmployeePayroll(1,"Ankush",55000,"M",new Date());
 console.log(employeePayroll.toString());
